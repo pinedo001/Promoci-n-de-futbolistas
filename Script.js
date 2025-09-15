87de8559277a335de101636d6432eb6e780143de
pinedo001 <juanmpinedorueda@gmail.com>
2025-09-15 09:19:03 -0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,6 @@
+// JS básico para futuro uso dinámico
+
+// Ejemplo: alert cuando se carga el sitio
+window.addEventListener('load', () => {
+  console.log("Página cargada. ¡Listo para subir jugadas increíbles!");
+});
